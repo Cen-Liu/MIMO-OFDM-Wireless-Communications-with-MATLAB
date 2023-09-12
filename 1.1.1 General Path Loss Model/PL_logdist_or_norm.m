23a86b3313a8708326fd53186f03299854b84f6f
LIU Cen <liucen@u.nus.edu>
2023-09-12 17:43:16 +0800

    Add files via upload

---
+++ 1.1.1 General Path Loss Model/PL_logdist_or_norm.m
@@ -0,0 +1,13 @@
+function PL = PL_logdist_or_norm(fc, d, d0, n, sigma)
+% Log-distance or Log-normal Shadowing Path Loss model
+% Input
+%       fc    : Carrier frequency[Hz]
+%       d     : Distance between base station and mobile station[m]
+%       d0    : Reference distance[m]
+%       n     : Path loss exponent
+%       sigma : Variance[dB]
+% Output
+%       PL    : Path loss[dB]
+lamda = 3e8/fc;
+PL = -20*log10(lamda/(4*pi*d0)) + 10*n*log10(d/d0);  % Eq.(1.4)
+if nargin>4,  PL = PL + sigma*randn(size(d));  end  % Eq.(1.5)
